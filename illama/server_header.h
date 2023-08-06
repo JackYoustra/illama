5359393eca76208bb0127a88e2c3d786428c4f82
Jack Youstra <jack@youstra.com>
2023-08-06 17:25:27 +0800

    Got loaded

---
+++ illama/server_header.h
@@ -0,0 +1,13 @@
+//
+//  server_header.h
+//  illama
+//
+//  Created by Jack Youstra on 8/6/23.
+//
+
+#ifndef server_header_h
+#define server_header_h
+
+int runServer(int argc, char **argv);
+
+#endif /* server_header_h */
