5fcb370cfc5fded16988b60738bd8f6df30c6ffe
Jack Youstra <jack@youstra.com>
2023-08-21 11:20:58 -0700

    Useful preview

--- illama/LlamaClient.swift
+++ illama/LlamaClient.swift
@@ -23,34 +23,40 @@ extension DependencyValues {
     }
     
     private enum LlamaClientKey: DependencyKey {
-        static let liveValue = {
-            let decoder = JSONDecoder()
-            return LlamaClient(
-                query: { q in
-                    try await LlamaInstance.shared.run_llama(prompt: q)
-                        .compactMap {
-                            try? decoder.decode(
-                                DataString.self,
-                                from: $0.data(using: .utf8)!
-                            )
-                        }
-                        .eraseToThrowingStream()
-                }
-            )
-        }()
-      static let testValue = LlamaClient(query: unimplemented())
-        static let previewValue: LlamaClient = LlamaClient { str in
-            chain(
-                    zip(
-                    Lorem.words(3)
-                        .split(separator: /\s/)
-                        .async,
-                    SuspendingClock()
-                        .timer(interval: .milliseconds(50))
-                ).map { DataString(content: String($0.0), stop: false) },
-                [DataString(content: "Done!", stop: true)].async
-            )
-            .eraseToThrowingStream()
-        }
+        static let liveValue: LlamaClient = LlamaClient.preview
+        static let testValue: LlamaClient = LlamaClient(query: unimplemented())
+        static let previewValue: LlamaClient = LlamaClient.preview
+    }
+}
+
+extension LlamaClient {
+    static let live: LlamaClient = {
+        let decoder = JSONDecoder()
+        return LlamaClient(
+            query: { q in
+                try await LlamaInstance.shared.run_llama(prompt: q)
+                    .compactMap {
+                        try? decoder.decode(
+                            DataString.self,
+                            from: $0.data(using: .utf8)!
+                        )
+                    }
+                    .eraseToThrowingStream()
+            }
+        )
+    }()
+    
+    static let preview: LlamaClient = LlamaClient { str in
+        chain(
+                zip(
+                Lorem.words(3)
+                    .split(separator: /\s/)
+                    .async,
+                SuspendingClock()
+                    .timer(interval: .milliseconds(50))
+            ).map { DataString(content: String($0.0), stop: false) },
+            [DataString(content: "Done!", stop: true)].async
+        )
+        .eraseToThrowingStream()
     }
 }
