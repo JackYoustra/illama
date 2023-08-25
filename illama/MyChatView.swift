7a92f48e23f20e742fc922cbc53a5b6de3b3f755
Jack Youstra <jack@youstra.com>
2023-08-25 16:03:25 -0700

    Change out way we do markdown formatting

--- illama/MyChatView.swift
+++ illama/MyChatView.swift
@@ -6,13 +6,66 @@
 //
 
 import SwiftUI
+import MarkdownUI
+
+enum MarkdownType: CaseIterable, Hashable, Identifiable {
+    case system
+    case markdownUI
+    case docC
+    case github
+    
+    var markdownTheme: MarkdownUI.Theme {
+        switch self {
+        case .docC: return .docC
+        case .github: return .gitHub
+        default:
+            return .basic
+        }
+    }
+    
+    var id: Self {
+        self
+    }
+}
+
+extension Optional where Wrapped == MarkdownType {
+    var systemImage: String {
+        switch self {
+        case .system:
+            return "applelogo"
+        case .markdownUI:
+            return "star"
+        case .docC:
+            return "doc"
+        case .github:
+            return "terminal"
+        case .none:
+            return "textformat"
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .system:
+            return "Apple"
+        case .markdownUI:
+            return "Enhanced"
+        case .docC:
+            return "DocC"
+        case .github:
+            return "GitHub"
+        case .none:
+            return "None"
+        }
+    }
+}
 
 private struct MarkdownSupport: EnvironmentKey {
-    static let defaultValue: Binding<Bool> = .constant(true)
+    static let defaultValue: Binding<MarkdownType?> = .constant(.markdownUI)
 }
 
 extension EnvironmentValues {
-    var markdownSupport: Binding<Bool> {
+    var markdownSupport: Binding<MarkdownType?> {
         get { self[MarkdownSupport.self] }
         set { self[MarkdownSupport.self] = newValue }
     }
@@ -21,7 +74,7 @@ extension EnvironmentValues {
 struct MyChatView: View {
     @Bindable var chat: Chat
     @State private var thing: String = ""
-    @State private var markdownSupport: Bool = true
+    @State private var markdownSupport: MarkdownType? = .markdownUI
 
     @Namespace var bottomID
     
@@ -57,8 +110,24 @@ struct MyChatView: View {
                 }
                 .submitLabel(.send)
         }.toolbar {
-            Toggle("🄼", isOn: $markdownSupport)
-                .padding(.trailing)
+            HStack(spacing: .zero) {
+                Menu {
+                    Picker("Markdown", selection: $markdownSupport) {
+                        Label("Apple", systemImage: MarkdownType.system.systemImage)
+                            .tag(Optional.some(MarkdownType.system))
+                        Label("Enhanced", systemImage: MarkdownType.markdownUI.systemImage)
+                            .tag(Optional.some(MarkdownType.markdownUI))
+                        Label("DocC", systemImage: MarkdownType.docC.systemImage)
+                            .tag(Optional.some(MarkdownType.docC))
+                        Label("GitHub", systemImage: MarkdownType.github.systemImage)
+                            .tag(Optional.some(MarkdownType.github))
+                        Label("None", systemImage: Optional<MarkdownType>.none.systemImage)
+                            .tag(Optional<MarkdownType>.none)
+                    }
+                } label: {
+                    Text("🄼↓") + Text(Image(systemName: markdownSupport.systemImage))
+                }
+            }
         }.environment(\.markdownSupport, $markdownSupport)
     }
 }
@@ -116,9 +185,14 @@ struct SingleMessageView: View {
     
     var textView: some View {
         Group {
-            if markdownSupport.wrappedValue {
+            let v = markdownSupport.wrappedValue
+            switch v {
+            case .markdownUI, .docC, .github:
+                Markdown(message.text)
+                    .markdownTheme(v?.markdownTheme ?? .basic)
+            case .system:
                 Text(LocalizedStringKey(message.text))
-            } else {
+            case .none:
                 Text(message.text)
             }
         }
