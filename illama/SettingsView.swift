3b252e5102ee54921025de2022613cd55b21cd83
Jack Youstra <jack@youstra.com>
2023-09-09 18:56:17 -0700

    Add about section

--- illama/SettingsView.swift
+++ illama/SettingsView.swift
@@ -20,6 +20,8 @@ struct SettingsView: View {
     
     enum SettingsPage: String, CaseIterable, Hashable, Identifiable {
         case markdown
+        case advanced
+        case about
         
         var id: Self {
             self
@@ -31,6 +33,10 @@ struct SettingsView: View {
             Picker("Settings Page", selection: $page) {
                 Text("Markdown")
                     .tag(SettingsPage.markdown)
+                Text("Advanced")
+                    .tag(SettingsPage.advanced)
+                Text("About")
+                    .tag(SettingsPage.about)
             }
             switch page {
             case .markdown:
@@ -47,7 +53,7 @@ struct SettingsView: View {
                     VStack {
                         Image(systemName: "exclamationmark.triangle")
                             .font(.system(size: 72.0))
-                        Text("Error")
+                        Text("Error loading sample")
                             .font(.title)
                         Text(failure.localizedDescription)
                             .font(.caption)
@@ -55,6 +61,24 @@ struct SettingsView: View {
                 case nil:
                     ProgressView()
                 }
+            case .advanced:
+                Text("Caution: Changing these settings could cause your app to crash.")
+                Text("🛠️ under construction, modification coming soon 🛠️")
+                Form(content: {
+//                    Slider(value: .constant(BundledModel.shared.contextSize), in: 512 ..< 4096, label: {
+//                        Text("Context Size")
+//                    })
+                    // Maybe stepper?
+                    LabeledContent("Context size", value: String(BundledModel.shared.contextSize))
+                    Toggle("mlock", isOn: .constant(BundledModel.shared.shouldMlock))
+                }).disabled(true)
+            case .about:
+                Text("""
+Hey! I'm Jack. My main job is working at [nanoflick](https://www.nanoflick.com/), but I wrote this app as a hackathon project and [wrote about it on my blog](https://www.jackyoustra.com/blog/llama-ios). I hope you enjoy it! It'll probably be tended to here and there, especially regarding the embarrasing inability to function on A12 chips and earlier. Before then, I hope you enjoy it!
+""")
+                Button("Cool, but can you fling a llama") {
+                    // TODO
+                }.hidden()
             }
         }.pickerStyle(.segmented)
         .task {
