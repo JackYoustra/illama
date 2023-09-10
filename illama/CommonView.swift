9251fc060602602d7576572710a75b1a43a83458
Jack Youstra <jack@youstra.com>
2023-09-09 18:57:09 -0700

    Add check for old phone

--- illama/CommonView.swift
+++ illama/CommonView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import DeviceKit
+import MessageUI
 
 final class BundledModel {
     static let shared = BundledModel()
@@ -38,7 +40,12 @@ let syncInitializationWork: () = {
     }
 }()
 
+let vowels: [Character] = ["a","e","i","o","u"]
+
 struct CommonView: View {
+    @State private var showWarning = false
+    @State private var hasShownWarning = false
+    
     var body: some View {
         Group {
             if #available(iOS 17.0, *) {
@@ -56,6 +63,38 @@ struct CommonView: View {
             }
         }.task {
             _ = syncInitializationWork
+        }.onAppear {
+            #if os(iOS)
+            if Device.current.cpu < .a13Bionic, !hasShownWarning {
+                // old
+                showWarning = true
+                hasShownWarning = true
+            }
+            #endif
+        }
+        .fullScreenCover(isPresented: $showWarning) {
+            VStack {
+                Text("⚠️ Old Phone 🦙☠️")
+                    .font(.largeTitle)
+                Text("Apple has a bug where I can't write code that makes 🦙 run at bearable speeds on A12-Bionic or older. Your phone has a\(Device.current.cpu.description.first.map(vowels.contains) == true ? "n" : "") \(Device.current.cpu.description), which is older. You can try running this on a newer device. Alternatively, you can try [emailing me](mailto:jack@youstra.com) and I can probably find a way. It's just quite hard and I don't know how many people are going to run into this problem yet, so I'm going to hold off until enough (probably just a few) people email me.")
+                Spacer()
+                Button("alt-f4 me right now", role: .destructive) {
+                    showWarning = false
+                    exit(0)
+                }
+                Button("I want to roll the dice and probably crash") {
+                    showWarning = false
+                }
+                Button("Email the developer, please") {
+                    showWarning = false
+                    let vc = MFMailComposeViewController()
+                    vc.setSubject("Please fix illama's A12 support")
+                    vc.setMessageBody("Hey, I'm writing because I really want to run illama on A12 processors or newer! Help!", isHTML: false)
+                    vc.setToRecipients(["jack@youstra.com"])
+                    UIApplication.shared.keyWindow!.rootViewController!.present(vc, animated: true)
+                }
+            }.buttonStyle(BorderedButtonStyle())
+            .padding()
         }
     }
 }
